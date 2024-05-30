cc35c39358fdb21b22d5ae595ecf1f30905b6b25
@itallonet <italloriancs@gmail.com>
2024-05-30 11:31:52 -0300

    caesar cipher

---
+++ Caesar.cs
@@ -0,0 +1,24 @@
+﻿namespace CaesarCipher;
+
+public static class Caesar
+{
+    public static string Encode(string input, int shift)
+    {
+        string output = "";
+
+        foreach (char character in input)
+        {
+            if (char.IsLetter(character))
+            {
+                char offset = char.IsUpper(character) ? 'A' : 'a';
+                output += (char)(((character + shift - offset) % 26) + offset);
+            }
+            else
+                output += character;
+        }
+
+        return output;
+    }
+
+    public static string Decode(string input, int shift) => Encode(input, 26 - shift);
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+![C#](https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white)
+
+# Caesar Cipher
+
+Simple and easy to use Caesar Cipher.
+## Functionalities
+
+- Encode
+- Decode
+
+
+## Demonstration
+
+```csharp
+// Hello to Ifmmp
+Caesar.Encode("Hello", 1)
+
+// Ifmmp to Hello
+Caesar.Decode("Ifmmp", 1)
+
+```
+## Author
+
+- [@itallonet](https://www.github.com/itallonet)
+
